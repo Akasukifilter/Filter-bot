ee764f432d4d72977f761e73eb71a7e74d471a9a
Akasukifilter <146570471+Akasukifilter@users.noreply.github.com>
2023-10-01 21:32:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 21295053
+API_HASH     = "297598578931dcc642c2519414079f8e"
+BOT_TOKEN    = "6198540082:AAGlC5T_nIR7ghPq21NwSf7_qE48Tmco1wY"
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://Arun:Arun@cluster0.zasxoq5.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
 ADMIN        = 1350642629
 CHANNEL      = "@GreyMatter_Bots"
