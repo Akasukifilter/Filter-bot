ccf44385ea9ebf8548b5aa0dfa2630fd016b4242
Akasukifilter <146570471+Akasukifilter@users.noreply.github.com>
2023-10-01 20:58:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 21295053
 API_HASH     = "297598578931dcc642c2519414079f8e"
 BOT_TOKEN    = "6198540082:AAGlC5T_nIR7ghPq21NwSf7_qE48Tmco1wY"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+DATABASE_URI = "mongodb+srv://Arun:Arun@cluster0.zasxoq5.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001628783106
+ADMIN        = 21295053
+CHANNEL      = "@Rose_filter_bot"
